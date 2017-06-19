1ad4bc049948e6a757e7c95d9b7771961e1dba4b
Charles Emrich and Jared Luedtke <charles+jared@>
2017-06-19 15:23:04 -0700

    add route get all info about the artist

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -16,6 +16,25 @@ namespace NickiMinAPI
         Console.WriteLine("Triggered dynamic route: " + parameters.page);
         return View[parameters.page + ".cshtml"];
       };
+      Get["/api/all"] = _ => {
+        List<Album> allAlbums = Albums.GetAll();
+        List<object> allResponse = new List <object> {};
+        foreach (Album album in allAlbums)
+        {
+          List<object> allTracks = new List<object> {};
+          foreach (Song song in album.GetSongs())
+          {
+            object newSong = new { title = song.Title, lyrics = song.Lyrics};
+            alltracks.Add(newSong);
+          }
+          object albumResponse = new { title = album.Title,
+            releaseDate = album.ReleaseDate.ToString(),
+            allTracks = albumTracks
+          };
+          allResponse.Add(albumResponse);
+        }
+        return allResponse;
+      };
       Get["/api/songs/{title}"] = parameters => {
         string title = parameters.title;
         Song foundSong = Song.Find(title);
@@ -40,9 +59,8 @@ namespace NickiMinAPI
                                    };
         return albumResponse;
       };
-      Get["/api/count"] = parameters => {
-        string title = parameters.title;
-        Song foundSong = Song.Find(title);
+      Get["/api/count"] = _ => {
+
         return foundSong;
       };
     }
