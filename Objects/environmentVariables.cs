c194f2a560e4b853121f9775ba11d0d09670aa20
Charles Emrich and Jared Luedtke <charles+jared@>
2017-06-22 15:33:21 -0700

    tweak ipsum generator, fix country filter

--- Objects/environmentVariables.cs
+++ Objects/environmentVariables.cs
@@ -6,8 +6,8 @@ namespace NickiMinAPI.Secrets
 {
   public class EnvVars
   {
-    public static string ConsumerSecret { get; } = "ekxMmFv8aR0dqixTqM6ywYJfz";
-    public static string ConsumerKey { get; } = "PJIkNE1XJfdJ1fdAhpJJRoufsiksd9oEyfjPPh41naYmO9aXpP";
+    public static string ConsumerSecret { get; } = "eeGt5Esi01am2tRB8zoU4p9fBi0HS0YRQYcAQnIWhJ2bUSz36u";
+    public static string ConsumerKey { get; } = "lzFrYcncTKYQgcGHvIIa9thgk";
     public static string AccessToken { get; } = "877392179320176640-s82A6qRILkIDlobCYFtBoB7B0MYQ6kr";
     public static string AccessTokenSecret { get; } = "E4ZgA4Jv68V33d0WomGhmuwEKIn15VSnu1ccaiZNuBgwn";
 
